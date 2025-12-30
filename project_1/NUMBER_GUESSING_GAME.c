a72eba90069b754bd986fc988248ce735d6f82ae
Abhay pandit <abhaytech12cy@gmail.com>
2025-12-31 00:53:35 +0530

    Update NUMBER_GUESSING_GAME.c

--- project_1/NUMBER_GUESSING_GAME.c
+++ project_1/NUMBER_GUESSING_GAME.c
@@ -3,6 +3,7 @@
 If you enter grater number than actual number ouput lower number please
 If you enter lower number than actual number ouput higher number please
 If you enter actual number then show the number*/
+//using Loop and random number generation 
 
 #include <stdio.h>
 #include <stdlib.h>
